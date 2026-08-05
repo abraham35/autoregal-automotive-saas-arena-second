7041435cc2656dbad430f755c036df99566b5510
Abraham Zewdu <90708198+abraham35@users.noreply.github.com>
2026-08-05 16:59:45 +0300

    Fix import paths for components in App.tsx
    Updated import paths to use lowercase for components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import Sidebar, { type ViewKey } from "./Components/Sidebar";
-import Topbar from "./Components/topbar";
-import AssetModal from "./Components/assetmodal";
-import VehicleDrawer from "./Components/vehicledrawer";
+import Sidebar, { type ViewKey } from "./components/Sidebar";
+import Topbar from "./components/Topbar";
+import AssetModal from "./components/AssetModal";
+import VehicleDrawer from "./components/VehicleDrawer";
 import Dashboard from "./views/Dashboard";
 import Fleet from "./views/Fleet";
 import BaysView from "./views/Bays";
